639d92fbfa086a9ca3b80379750a398b8e1bc87d
Omar Faizyar <omar@Omars-MBP.inc.jfrog.local>
2016-06-17 16:47:51 -0700

    def2
    def2

--- diag.py
+++ diag.py
@@ -6,13 +6,16 @@ import argparse
 from colorama import Fore, Back, Style
 
 artifactory = sys.argv[1]
+twohunerr = 0
+threehunerr = 0
+fourhunerr = 0
 
 i = 0
 with open(artifactory + "/logs/artifactory.log") as f:
     for line in f:
     	if "INFO" in line:
     		try:
-    			regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
+    	   		regex = '^([-0-9]+ [:0-9]+,[0-9]+) \[([-a-zA-Z0-9]+)\] \[([A-Z]+) *\] \(([.a-zA-Z0-9]+):([0-9]+)\) - (.*)$'
     			match = re.search(regex,line.encode('ascii'),flags = 0)
     			print Fore.GREEN + "INFO:" + (match.group(6))
            
@@ -28,7 +31,12 @@ with open(artifactory + "/logs/request.log") as f:
             artdate = match.group(1)[4:]
             artdate = artdate[:-2]
             a,b,c,d,e,f,g,h = artdate
-            print  match.group(9)
+            if match.group(9).startswith("2"):
+                    twohunerr += 1
+            if match.group(9).startswith("3"):
+                    threehunerr += 1
+            if match.group(9).startswith("4"):
+                    fourhunerr += 1
 
             #group4 = IP
             #group1 = time
@@ -37,7 +45,11 @@ with open(artifactory + "/logs/request.log") as f:
             #group7 = the resource access
             #group9 = HTTP response code
         except Exception:
-            pass
+            print Exception
+print "200 Codes: "  (twohunerr)
+print "300 Codes: "  (threehunerr)
+print "400 Codes: "  (fourhunerr)
+
 
     	
  
